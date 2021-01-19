e64769835d84e1ca7073f714383fc87c6cb42df2
DTag <Dentag@mail.ru>
2021-01-19 16:36:01 +0300

    First lesson hw completed

---
+++ app/src/main/java/com/example/noteapp/ui/main/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.noteapp.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import com.example.noteapp.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+    private lateinit var adapter: MainAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        adapter = MainAdapter()
+        val mainRecycler = findViewById<RecyclerView>(R.id.mainRecycler)
+        mainRecycler.adapter = adapter
+
+        viewModel.viewState().observe(this,
+            { t ->
+                t?.let { adapter.notes = it.notes }
+            })
+    }
+}
